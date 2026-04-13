1a2a6389bfe0f6840ac4e26bca1d7f80c1f6b54b
Minedscorpion <69402472+MinedscorpionBR@users.noreply.github.com>
2026-04-13 16:58:12 -0300

    Add user input for name, age, and city

---
+++ Mensagem.py
@@ -0,0 +1,10 @@
+# Lendo texto (sempre retorna string)
+nome = input("Digite seu nome: ")
+
+# Lendo número inteiro
+idade = int(input("Digite sua idade: "))
+
+# Lendo número real
+cidade = input("Digite sua cidade: ")
+
+print(f"Olá {nome}, você tem {idade} anos e você mora em: {cidade}, eu preciso de um pix de 500R$.")
