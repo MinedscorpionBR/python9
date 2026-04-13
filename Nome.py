57366123983a82fc8d15b22d9bc90852e8fb7826
Minedscorpion <69402472+MinedscorpionBR@users.noreply.github.com>
2026-04-13 17:20:15 -0300

    Add basic name input and string manipulations

---
+++ Nome.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome completo: ")
+print(f"Bem-vindo(a), {nome},e o pix!?")
+
+print(f"O nome {nome} tem {len(nome)} caracteres")
+print(f"Primeira letra: {nome[0]}")
+print(f"Última letra: {nome[-1]}")
+print(f"{nome.upper()}")
