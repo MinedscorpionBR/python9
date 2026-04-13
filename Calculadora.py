0333e935a0e6f7c9af76789646b8b5c9787aa9ff
Minedscorpion <69402472+MinedscorpionBR@users.noreply.github.com>
2026-04-13 17:04:46 -0300

    Add basic calculator functionality in Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,6 @@
+# Lendo números inteiros
+num1 = int(input("Digite um número inteiro: "))
+soma1 = num1 * 2
+soma2 = num1 / 2
+print(f"esse numero vezes 2 é: {soma1}")
+print(f"esse numero dividido por 2 é: {soma2}")
